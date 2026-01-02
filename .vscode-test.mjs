9b74b4f57d3e1a5f0e541dcaf65d6c4f0ff07613
Tommy Woodley <thwoodle+gh@cisco.com>
2026-01-02 10:50:15 +0000

    use vscode runner

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -2,4 +2,8 @@ import { defineConfig } from '@vscode/test-cli';
 
 export default defineConfig({
 	files: 'out/test/**/*.test.js',
+	coverage: {
+		include: ['out/**/*.js'],
+		exclude: ['out/test/**', 'out/webview-ui/**'],
+	},
 });
